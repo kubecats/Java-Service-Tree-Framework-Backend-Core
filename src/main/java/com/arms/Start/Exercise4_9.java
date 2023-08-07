fc44784ad3a42568f93b2e49c1113987ef829569
lhs <lhs.shcard@gmail.com>
2023-08-07 14:00:47 +0900

    feat: [JAVA-1] #Ex4_9 - 23.08.07 #close #time 1h +review SR @kubecats [ Backend-Core::Java-Service-Tree-Framework(JSTF)::kubecats] [Requirement Manage] http://www.a-rms.net [Document] http://www.313.co.kr/confluence [IssueTracker] http://www.313.co.kr/jira [VersionControl] https://github.com/313devgrp [CodeReview] http://www.313.co.kr/fecru [CICD-Deploy] http://www.313.co.kr/jenkins [BuildManager] http://www.313.co.kr/spinnaker [ArtifactManager] http://www.313.co.kr/nexus [CodeAnalysis] http://www.313.co.kr/sonar [Docker Hub] https://hub.docker.com/u/313devgrp [Kubernetes] http://www.313.co.kr:31380 [DockerSwarm] http://www.313.co.kr/portainer/#!/auth [DB Admin] http://www.313.co.kr/phpmyadmin/ [S3 Admin] http://www.313.co.kr/minio/login [MEM Admin] http://www.313.co.kr/redis/ [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true [Kibana] http://www.313.co.kr/kibana/app/home#/ [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty [ZipKin] http://www.313.co.kr/zipkin/ [ElasticHQ] http://www.313.co.kr/elastichq/#!/ [Grafana] http://www.313.co.kr/grafana [NAS] http://www.313.co.kr:5000/webman/index.cgi [Mail] http://www.313.co.kr/mail/ [Auth] http://www.313.co.kr/auth/ [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/Start/Exercise4_9.java
@@ -0,0 +1,15 @@
+package com.arms.Start;
+
+public class Exercise4_9 {
+    public static void main(String[] args) {
+        String str = "12345";
+        int sum = 0;
+
+        for(int i=0; i<str.length(); i++){
+
+        }
+        System.out.println("sum:"+sum);
+    }
+}
+
+//[Hint] String클래스의 charAt(int i)을 사용
